79761a2a42b944db79a76314b92a0163a9906046
GaganaGavesh <gaveshgamage@gmail.com>
2021-07-08 15:10:51 +0530

    Removed unnecessary logics

--- src/components/SearchBarSelectComponent.tsx
+++ src/components/SearchBarSelectComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import AsyncSelect from "react-select/async";
 import makeAnimated from "react-select/animated";
 import { refChildUsers } from '../firebase/firebase';
@@ -11,22 +11,6 @@ const AsyncSearchBar = (props: any) => {
   const [collabs, setCollabs] = useState("");
   const animatedComponents = makeAnimated();
 
-  const fetchCurrentUser: any = [];
-  // useEffect(()=>{
-  //       refChildUsers.orderByChild("userId").limitToFirst(1).equalTo("AJv5bZn6aEeIL3SDutVeoBVrR8F3").once("value").then((snapshot: any) => {
-  //         if (snapshot.exists()) {
-  //           snapshot.forEach((childSnapshot: any) => {
-  //             fetchCurrentUser.push({
-  //               id: childSnapshot.key,
-  //               ...childSnapshot.val()
-  //             });
-  //           });
-  //           setCurrentUser(fetchCurrentUser);
-  //           console.log('currentUser',currentUser);
-  //         }
-  //       });
-  // },[fetchCurrentUser]);
-  
   const loadOptions = () => {
     return refChildUsers.orderByChild("displayName").startAt(query).endAt(`${query}\uf8ff`).once("value").then((snapshot: any) => {
       if (snapshot.exists()) {
@@ -34,22 +18,11 @@ const AsyncSearchBar = (props: any) => {
         const fetchUser: any = [];
 
         snapshot.forEach((childSnapshot: any) => {
-          console.log('Child Snapshot', childSnapshot.val().follows)
-          let isFollows = false;
-          childSnapshot.val().follows.forEach((followsId: string) => {
-            if (followsId === 'AJv5bZn6aEeIL3SDutVeoBVrR8F3') {
-              isFollows = true;
-            }
-          });
-          console.log('Is follows', isFollows);
-
           fetchUser.push({
             id: childSnapshot.key,
-            isFollows: isFollows,
             ...childSnapshot.val()
           });
         });
-        console.log('User', fetchUser)
         return fetchUser;
       }
     });
